6cfc20f55d597e45c61f82affa4e782bf6eacf50
wdx <872386515@qq.com>
2020-04-03 15:16:19 +0800

    Initial commit

---
+++ src/main/java/com/docker/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String helloWord(){
+        return "sadddddddddddddddddddddddddd";
+    }
+}
